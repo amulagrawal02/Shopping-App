2c60439f6aad73346ea78a172bb9369f17c36623
Amul Agrawal <amulagrawal32@gmail.com>
2021-10-05 19:24:25 +0530

    connect frontend and backend
    Signed-off-by: Amul Agrawal <amulagrawal32@gmail.com>

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const seedDB = require("./seed");
+
+mongoose
+  .connect("mongodb://localhost:27017/shopping-app")
+  .then(() => {
+    console.log("DB is connected");
+  })
+  .catch((err) => {
+    console.log("DB not connected", err);
+  });
+
+// seedDB(); /* run this file only one time  */
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("hello from server");
+});
+
+app.listen(3003, () => {
+  console.log("server start at server 3003");
+});
