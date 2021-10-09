d07bb49bf09f3ea0d9eddb541da36782ad65bd2d
Amul Agrawal <amulagrawal32@gmail.com>
2021-10-09 19:25:14 +0530

    Restructuring the folders
    Signed-off-by: Amul Agrawal <amulagrawal32@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import ProductList from "./components/ProductList";
-import { Container } from "react-bootstrap";
+import ProductList from "./components/products/ProductList";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import New from "./components/New";
-import Show from "./components/Show";
-import Edit from "./components/Edit";
+import New from "./components/pages/New";
+import Show from "./components/pages/Show";
+import Edit from "./components/pages/Edit";
+import Layout from "./components/layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <Container className="main-container">
-          <Switch>
-            <Route exact path="/products" component={ProductList}></Route>
-            <Route exact path="/products/new" component={New}></Route>
-            <Route exact path="/products/:id" component={Show}></Route>
-            <Route exact path="/products/:id/edit" component={Edit}></Route>
-
-          </Switch>
-        </Container>
+        <Layout />
+        <Switch>
+          <Route exact path="/products" component={ProductList}></Route>
+          <Route exact path="/products/new" component={New}></Route>
+          <Route exact path="/products/:id" component={Show}></Route>
+          <Route exact path="/products/:id/edit" component={Edit}></Route>
+        </Switch>
       </div>
     );
   }
---
+++ frontend/src/components/layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import { Container } from "react-bootstrap";
+import "./Layout.css";
+
+function Layout(props) {
+  return (
+    <div>
+      <Header />
+      <main className="main-container">
+        <Container>{props.children}</Container>
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
